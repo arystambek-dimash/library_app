dc7d0c10962d2e148a5a8d88b2e5e26847f45d1f
dinmukhammed <arystambekdimash005@gmail.com>
2024-03-15 22:50:56 +0500

    created router registration and logout

--- api/users/views.py
+++ api/users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from api.users.serializers import UserSerializer
+
+
+@api_view(['POST'])
+def register(request: Request):
+    try:
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            access_token = refresh.access_token
+
+            data = {
+                'refresh': str(refresh),
+                'access': str(access_token),
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['POST'])
+def logout(request: Request):
+    try:
+        refresh = request.data.get('refresh')
+        try:
+            token = RefreshToken(token=refresh)
+        except Exception as e:
+            return Response("Invalid token", status=status.HTTP_400_BAD_REQUEST)
+
+        token.blacklist()
+        return Response({"status": "Logged out"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
