8baa954d11768d7bfd0218607dba3b727c1ae317
dinmukhammed <arystambekdimash005@gmail.com>
2024-03-15 22:47:59 +0500

    fixed name apps

--- api/networks/apps.py
+++ api/networks/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SocialsConfig(AppConfig):
+class NetworksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'socials'
+    name = 'api.networks'
