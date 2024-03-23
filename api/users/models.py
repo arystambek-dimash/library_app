b1e5a9a1bc6e82b356bfbcf2599db7b20775f06f
dinmukhammed <arystambekdimash005@gmail.com>
2024-03-23 21:54:48 +0500

    feat: removed role models

--- api/users/models.py
+++ api/users/models.py
@@ -1,37 +1,48 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 
-class Role(models.Model):
-    ADMIN = 1
-    CLIENT = 2
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    ROLE_CHOICES = (
-        (ADMIN, 'Admin'),
-        (CLIENT, 'Client')
-    )
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=CLIENT)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, name, password, verified=True, **extra_fields)
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     username = None
     first_name = None
     last_name = None
     name = models.CharField(max_length=32)
-    email = models.EmailField(max_length=100,
-                              unique=True,
-                              verbose_name='email address',
-                              null=False,
-                              error_messages={
-                                  "unique": "A user with that email already exists.",
-                              },
-                              )
-    password = models.CharField(max_length=128)
-    role = models.ForeignKey(Role, on_delete=models.DO_NOTHING, default=2)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    verified = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'password']
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.name
